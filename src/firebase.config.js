ba41dc6ab43ea7d3e2ec11465a7fa1235b186941
mwiin561 <mwiineirwin23@gmail.com>
2023-11-16 14:30:15 +0300

    restaurant stuff

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -3,13 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD_bxGG0WgOKDejZvER2fTq2xZZjfUs8zs",
-  authDomain: "restaurantapp-c2ed6.firebaseapp.com",
-  databaseURL: "https://restaurantapp-c2ed6-default-rtdb.firebaseio.com",
-  projectId: "restaurantapp-c2ed6",
-  storageBucket: "restaurantapp-c2ed6.appspot.com",
-  messagingSenderId: "174416156605",
-  appId: "1:174416156605:web:2ec169ea4ef3e7bb25e4d4",
+  apiKey: "AIzaSyBRNaPHlPiQ0bhJu2ffd33TlHvolFr8Akw",
+  authDomain: "restaurant-app-1b234.firebaseapp.com",
+  projectId: "restaurant-app-1b234",
+  storageBucket: "restaurant-app-1b234.appspot.com",
+  messagingSenderId: "641424263542",
+  appId: "1:641424263542:web:150ce5f0a33465b194d257"
 };
 
 const app = getApps.length > 0 ? getApp() : initializeApp(firebaseConfig);
